5e8612f12a8464c32c0301553876073a3b1cebe5
Lucas Bishop <bishoplucas95@gmail.com>
2020-05-06 15:57:41 -0400

    parsed metadata

--- code/parse_metadata.R
+++ code/parse_metadata.R
@@ -6,14 +6,25 @@ files <- read_tsv("data/mothur_output/stability.paired.files",
 
 shared <- read.delim("data/mothur_output/final.0.03.subsample.shared", 
                      header = T, stringsAsFactors = F) %>% 
-  select(-label, -numOtus)
+  select(-label, -numOtus) %>% filter(!grepl("PCRwater", Group) & !grepl("empty", Group) & !grepl("bad", Group))
 
-# Build first meta data file
-temp_meta <- files %>% filter(!grepl("PCRwater", filename) & !grepl("empty", filename)) %>% select(-r2_fastq) %>% 
-  separate(col = r1_fastq,
-          into = c("sampleid", "kit", "storage", "samplenum", "x1", "x2", "x3", "x4"), sep = "_") %>% select(-contains("x"))
+# Build first metadata file 
+temp_meta <- files %>% filter(!grepl("PCRwater", filename) & !grepl("empty", filename) & !grepl("bad", filename)) %>% separate(col = r1_fastq,
+          into = c("kit", "storage", "stool_id", "x1", "x2", "x3", "x4"), sep = "_", remove = FALSE) %>% 
+  select(filename, kit, storage, stool_id) %>% rename(Group = filename)
+  
 
 #Need to parse what each storage and kit abbreviation mean
+# Kits: Zymobiomics (z), PowerMag Microbiome (pm), PowerSoil (ps)
+# Storage: Room Temperature (rt), Zymo DNA/RNA Shield (zdr), 
+#         frozen only (f), Omnigene Genotek (og)
+
+metadata <- temp_meta %>% mutate(kit = str_replace_all(string = temp_meta$kit, 
+                                                       pattern = c("z" = "Zymobiomics","pm" = "PowerMag", "ps" = "PowerSoil")),
+                                 storage = str_replace_all(string = temp_meta$storage,
+                                                           pattern = c("rt" = "RoomTemp", "zdr" = "Zymo_DNARNA_Shield", "f" = "Frozen", "og" = "Omnigene_Genotek")))
+
+
 #need to look at plate map to see if any can be excluded
 #Need to look at the samplenum key that Marc gave me to make sure everything matches 
 
