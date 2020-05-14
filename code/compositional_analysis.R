bbf077891a69c125f048001447bd5198e548bb97
Lucas Bishop <bishoplucas95@gmail.com>
2020-05-14 16:29:08 -0400

    added code to generate prelim figure on genus level differences by kit

--- code/compositional_analysis.R
+++ code/compositional_analysis.R
@@ -3,8 +3,9 @@ library(tidyverse)
 
 ## basic diversity analysis
 shared <- read_tsv("data/mothur_output/final.0.03.subsample.shared", col_types=cols(Group=col_character()))
-metadata <- read_csv("data/final.metadata.csv") %>% ifelse(metadata$storage == "ext", mutate())
-taxonomy <- read_tsv("data/mothur_output/final.taxonomy") 
+metadata <- read_csv("data/final.metadata.csv") %>% 
+  filter(kit == "PowerMag" | kit == "PowerSoil" | kit == "Zymobiomics")
+taxonomy <- read_tsv("data/mothur_output/final.taxonomy") %>% 
   rename_all(tolower) %>% 
   # Split taxonomic information into separate columns for each taxonomic level  
   mutate(taxonomy=str_replace_all(taxonomy, c("\\(\\d*\\)" = "", #drop digits with parentheses around them
@@ -31,12 +32,24 @@ phylum_taxa <- phylum_data %>%
 top_genus <- phylum_data %>% group_by(genus) %>% 
   summarize(agg_rel_abund=sum(relabund)) %>% 
   arrange(desc(agg_rel_abund)) %>% 
-  top_n(n=10, agg_rel_abund) %>% 
+  top_n(n=9, agg_rel_abund) %>% 
   pull(genus)
 
+genus_taxa <- phylum_data %>% 
+  group_by(Group, genus) %>% 
+  summarize(agg_rel_abund = sum(relabund)) %>% 
+  inner_join(., metadata, by = 'Group') %>% 
+  ungroup()
 
-
-
+# needs improvement but general idea is there
+genus_taxa %>% filter(genus %in% top_genus) %>% 
+ggplot(aes(x= kit, y=agg_rel_abund))+
+  geom_hline(yintercept = 0, linetype = "dashed") + 
+  geom_jitter(width = 0.3) + 
+  facet_wrap(~genus) +
+  labs(title=NULL, 
+       y="Relative abundance (%)")+
+  theme(axis.text.x = element_text(angle=45, hjust=1))
 
 
 
